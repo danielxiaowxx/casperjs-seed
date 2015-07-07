a51d1eb0c0230d998b55e6483f4f5d53cfb30786
danielxiaowxx <xiaohutu136@163.com>
2015-07-07 19:53:35 +0800

    remove some not project files

--- tests/gmc-order/product-info.js
+++ tests/gmc-order/product-info.js
@@ -20,6 +20,10 @@ var steps = [
   {
     title: '选择物流方式',
     fn: step3
+  },
+  {
+    title: '更改目标城市',
+    fn: step4
   }
 ];
 
@@ -64,6 +68,7 @@ function step2(test, title) {
 function step3(test, title) {
   casper.then(function() {
 
+    // 打开可选物流
     this.evaluate(function() {
       $('a[data-logisitcs-selector]').click();
     });
@@ -77,10 +82,60 @@ function step3(test, title) {
 
       test.assertEval(function() {
         // 有可选的物流方式
-        return __utils__.findAll('#shipping-delivery tr').length > 0;
+        return __utils__.findAll('#shipping-delivery tbody tr').length > 0;
       }, title);
 
-      casper.captureSelector(config.screenshotsDir + '/step3.png', '#shipping-delivery');
+      // 切换任何一种物流方式
+      this.evaluate(function() {
+        var $dom = $('#shipping-delivery');
+        $dom.find('tbody tr[class!="selected"]').click();
+        $dom.find('.confirm.button-yellow').click();
+      });
+
+      casper.captureSelector(config.screenshotsDir + '/step3-1.png', '#shipping-delivery');
+
+      this.waitForResource(/http:\/\/www\.globalmarket\.com\/gmc-order\/gmportal\/order\/component\/product\/\d+\.gm.*/, function then() {
+
+        test.assertExists('#product-name', title + '页面数据更新');
+
+        casper.capture(config.screenshotsDir + '/step3-2.png');
+
+      }, function timeout() {
+        this.log(title + '数据更新失败', 'error').exit();
+      });
+
+
+    }, function timeout() {
+      this.log(title + '加载失败', 'error').exit();
+    });
+
+  });
+}
+
+function step4(test, title) {
+  casper.then(function() {
+
+    // 打开国家选择器
+    this.evaluate(function() {
+      $('#address-selector').click();
+    });
+
+    casper.waitFor(function check() {
+      return this.evaluate(function() {
+        var $dom = $('#shipping-address');
+        return $dom.css('display') == 'block' && $dom.css('opacity') == 1;
+      });
+    }, function then() {
+
+      casper.captureSelector(config.screenshotsDir + '/step4-1.png', '#shipping-address');
+
+      // 切换到随意一个国家
+      this.evaluate(function() {
+        var $dom = $('#shipping-address');
+        $dom.find('.first-select').click();
+      });
+
+      casper.captureSelector(config.screenshotsDir + '/step4-2.png', '#shipping-delivery');
     }, function timeout() {
       this.log(title + '加载失败', 'error').exit();
     });
