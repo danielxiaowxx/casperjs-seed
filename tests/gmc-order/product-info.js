5f1fc64fae2a23f85b8551bf6b9ba1ec38c3b82a
danielxiaowxx <xiaohutu136@163.com>
2015-07-10 09:50:46 +0800

    add steps

--- tests/gmc-order/product-info.js
+++ tests/gmc-order/product-info.js
@@ -24,6 +24,10 @@ var steps = [
   {
     title: '更改目标城市',
     fn: step4
+  },
+  {
+    title: '添加到购物车',
+    fn: step5
   }
 ];
 
@@ -132,10 +136,69 @@ function step4(test, title) {
       // 切换到随意一个国家
       this.evaluate(function() {
         var $dom = $('#shipping-address');
-        $dom.find('.first-select').click();
+        var $country = $dom.find('.form-select').eq(0);
+        $country.find('.form-select-input').click();
+        $country.find('.form-select-list a:last').click();
       });
 
-      casper.captureSelector(config.screenshotsDir + '/step4-2.png', '#shipping-delivery');
+      // 选择随意一个省份
+      this.waitForResource(/fetch\.gm.*/, function then() {
+
+        this.evaluate(function() {
+          var $dom = $('#shipping-address');
+          var $district = $dom.find('.form-select').eq(1);
+          $district.find('.form-select-input').click();
+          $district.find('.form-select-list a:last').click();
+        });
+
+      }, function timeout() {
+        this.log(title + '省份数据加载失败', 'error').exit();
+      });
+
+      // 选择随意一个城市
+      this.waitForResource(/fetch\.gm.*/, function then() {
+
+        this.evaluate(function() {
+          var $dom = $('#shipping-address');
+          var $city = $dom.find('.form-select').eq(2);
+          $city.find('.form-select-input').click();
+          $city.find('.form-select-list a:last').click();
+          $dom.find('.confirm').click();
+        });
+
+      }, function timeout() {
+        this.log(title + '省份数据加载失败', 'error').exit();
+      });
+
+      this.waitForResource(/http:\/\/www\.globalmarket\.com\/gmc-order\/gmportal\/order\/component\/product\/\d+\.gm.*/, function then() {
+
+        test.assertExists('#product-name', title + '页面数据更新');
+
+        casper.capture(config.screenshotsDir + '/step4-2.png');
+
+      }, function timeout() {
+        this.log(title + '数据更新失败', 'error').exit();
+      });
+
+
+    }, function timeout() {
+      this.log(title + '加载失败', 'error').exit();
+    });
+
+  });
+}
+
+function step5(test, title) {
+  casper.then(function() {
+
+    this.evaluate(function() {
+      $('a[data-submit-btn="add-to-cart"]').click();
+    });
+
+    this.waitForSelector('#edit-cart-btn', function then() {
+      test.assertExists('#edit-cart-btn', title);
+      this.click('#edit-cart-btn');
+      casper.capture(config.screenshotsDir + '/step5.png');
     }, function timeout() {
       this.log(title + '加载失败', 'error').exit();
     });
@@ -145,7 +208,7 @@ function step4(test, title) {
 
 /*========== Main ==================================================*/
 
-common.runTest('Test gmc-order product info page', 3, steps);
+common.runTest('Test gmc-order product info page', 6, steps);
 
 
 
